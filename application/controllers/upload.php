9a575c95b381faa4317588605352cc5e79005377
John Laniba <johnlan@live.com>
2011-08-08 15:37:01 +0800

    Integrating jquery fileuploader version 1.3 into codeigniter

---
+++ application/controllers/upload.php
@@ -0,0 +1,42 @@
+<?php
+
+class Upload extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+	}
+
+	function index()
+	{
+		$this->load->view('upload_form', array('error' => ' ' ));
+	}
+
+	function do_upload()
+	{
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '1000';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			echo '<div id="status">error</div>';
+			echo '<div id="message">'. $this->upload->display_errors() .'</div>';
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+			echo '<div id="status">success</div>';
+			//then output your message (optional)
+			echo '<div id="message">'. $data['upload_data']['file_name'] .' Successfully uploaded.</div>';
+			echo '<pre>';
+			print_r($data);
+		}
+	}
+}
+?>
---
+++ application/views/upload_form.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>jQuery File Uploader</title>
+<base href="<?php echo substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'], "/")+1); ?>" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+
+<style type="text/css">
+body {
+	margin: 10px;
+	font: 62% Tahoma, Arial, sans-serif;
+}
+#main_container{
+	font-size: 1.4em;
+}
+h2 {
+	font-size: 2em;
+	padding-bottom: 20px;
+}
+</style>
+
+<link href="css/ui-lightness/jquery-ui-1.8.14.custom.css" rel="stylesheet" type="text/css" />
+<link href="css/fileUploader.css" rel="stylesheet" type="text/css" />
+
+<script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
+<script src="js/jquery-ui-1.8.14.custom.min.js" type="text/javascript"></script>
+<script src="js/jquery.fileUploader.js" type="text/javascript"></script>
+
+</head>
+<body>
+
+<div id="main_container">
+	<h2>jQuery Fileuploder Plugin</h2>
+	
+	<form action="index.php/upload/do_upload" method="post" enctype="multipart/form-data">
+	
+		<input type="file" name="userfile" class="fileUpload" multiple>
+				
+		<button id="px-submit" type="submit">Upload</button>
+		<button id="px-clear" type="reset">Clear</button>
+	
+	</form>
+	
+	<script type="text/javascript">
+		jQuery(function($){
+			$('.fileUpload').fileUploader();
+		});
+	</script>
+	
+</div>
+
+</body>
+</html>
